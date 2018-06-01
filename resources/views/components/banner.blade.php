a293386c282b0fa8c71b2a7301d5e1bd94547b5d
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-01 16:05:42 +0700

    [INIT] Internship Learning Code

---
+++ resources/views/components/banner.blade.php
@@ -0,0 +1,19 @@
+<div class="row">
+    <div class="col-12">
+        <video src=""></video><p>video for Krungsri Prime</p>
+    </div>
+</div>
+<div class="row scroll-index">
+    <div class="col-12 text-center">
+        <div class="row">
+            <div class="col-12">
+                <img src="/imgs/down.png" alt="scroll-down vector">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <p class="scroll-text">SCROLL DOWN</p>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Krungsri Prime</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href={{ asset('/css/index.css') }}>
+    </head>
+    <body>
+        @include('utils.navbar')
+        @include('components.banner')
+        @include('components.youngblock')
+    </body>
+</html>
