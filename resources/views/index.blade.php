57256294fdcd267a63259ab12f49c86d08cda12c
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-01 17:06:04 +0700

    {ADD} master : add font-face into web

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,9 @@
         <meta charset="UTF-8">
         <title>Krungsri Prime</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href={{ asset('/css/index.css') }}>
+        <link rel="stylesheet" href={{ asset('/css/font.css') }}>
+        <link rel="stylesheet" href={{ asset('/css/index.css') }}>
+        
     </head>
     <body>
         @include('utils.navbar')
