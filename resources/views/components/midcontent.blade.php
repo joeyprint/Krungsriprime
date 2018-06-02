9d604b9ef607b820a8213d8e9c74bb9472417c0d
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-02 10:36:40 +0700

    [ADD] master : add middle content and create style

---
+++ resources/views/components/midcontent.blade.php
@@ -0,0 +1,17 @@
+<section id="midcontent">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8 text-center line margin10">
+                <p class="margin20 text">
+                    ร่วมแชร์โมเม้นต์ก้าวที่ล้าน ที่เป็นก้าวแห่งการเติบโต<br>
+                    และก้าวแรกของความสำเร็จของคุณ เพียงแชร์เรื่องราวพร้อมติดแฮชแท็ก<br>
+                    <span class="yellow-text text">
+                        # F <span class="white-text">I</span> R S T M I L L I O N
+                    </span>
+                </p>
+
+            </div>
+        </div>
+    </div>
+</section>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,11 +6,13 @@
         <link rel="stylesheet" href={{ asset('/css/font.css') }}>
         <link rel="stylesheet" href={{ asset('/css/index.css') }}>
         <link rel="stylesheet" href={{ asset('/css/youngblock.css') }}>
+        <link rel="stylesheet" href={{ asset('/css/midcontent.css') }}>
     </head>
     <body>
         @include('utils.navbar')
         @include('components.banner')
         @include('components.youngblock')
+        @include('components.midcontent')
         @include('utils.footer')
     </body>
 </html>
