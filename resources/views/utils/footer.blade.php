542215a2427bef1333e41f803fdedcfa91817e69
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-03 00:39:26 +0700

    [ADD] master : Add footer into page

---
+++ resources/views/utils/footer.blade.php
@@ -0,0 +1,7 @@
+<section id="footer">
+    <div class="row">
+        <div class="col-12 text-center yellowbg">
+            <p class="black-text padding10 margin0">Copyright &copy; Krungsri prime</p>
+        </div>
+    </div>
+</section>
