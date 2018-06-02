30df2785549b482cdda09f5bae23b605e4389cec
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-02 18:14:21 +0700

    [EDIT] master : edit class name for young block

--- resources/views/components/youngblock.blade.php
+++ resources/views/components/youngblock.blade.php
@@ -1,30 +1,30 @@
 <section id="youngblock">
     <div class="container">
         <div class="row">
-            <div class="col-4 margin100">
+            <div class="col-4 marginTop100">
                 <img class="block-shadow" src={{ asset('/imgs/people1.png') }} alt="people 2">
             </div>
-            <div class="col-4 margin15">
+            <div class="col-4 marginTop15">
                 <img class="block-shadow" src={{ asset('/imgs/people1.png') }} alt="people 1">
             </div>
-            <div class="col-4 margin100">
+            <div class="col-4 marginTop100">
                 <img class="block-shadow" src={{ asset('/imgs/people1.png') }} alt="people 3">
             </div>
         </div>
         <div class="row">
-            <div class="col-4 margin15">
+            <div class="col-4 marginTop15">
                 <img class="block-shadow" src={{ asset('/imgs/people1.png') }} alt="people 5">
             </div>
-            <div class="col-4 margin15 position-bot85">
+            <div class="col-4 marginTop15 position-bot85">
                 <img class="block-shadow" src={{ asset('/imgs/people1.png') }} alt="people 4">
             </div>
-            <div class="col-4 margin15">
+            <div class="col-4 marginTop15">
                 <img class="block-shadow" src={{ asset('/imgs/people1.png') }} alt="people 6">
             </div>
         </div>
         <div class="row">
             <div class="col-4"></div>
-            <div class="col-4 margin15 position-bot85">
+            <div class="col-4 marginTop15 position-bot85">
                 <img class="block-shadow" src={{ asset('/imgs/people1.png') }} alt="people 7">
             </div>
             <div class="col-4"></div>
