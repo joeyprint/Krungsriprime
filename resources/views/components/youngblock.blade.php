60cae12bd5699f5ce8920221b45d6c77beffa960
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-04 14:50:49 +0700

    [EDIT] master : edit structure code in young block component

--- resources/views/components/youngblock.blade.php
+++ resources/views/components/youngblock.blade.php
@@ -1,6 +1,6 @@
 <section id="youngblock">
     <div class="container">
-        <div class="row">
+        <div class="row justify-content-center">
             <div class="col-4 marginTop100">
                 <img class="block-shadow" src={{ asset('/imgs/people1.png') }} alt="people 2">
             </div>
@@ -10,8 +10,6 @@
             <div class="col-4 marginTop100">
                 <img class="block-shadow" src={{ asset('/imgs/people1.png') }} alt="people 3">
             </div>
-        </div>
-        <div class="row">
             <div class="col-4 marginTop15">
                 <img class="block-shadow" src={{ asset('/imgs/people1.png') }} alt="people 5">
             </div>
@@ -21,13 +19,9 @@
             <div class="col-4 marginTop15">
                 <img class="block-shadow" src={{ asset('/imgs/people1.png') }} alt="people 6">
             </div>
-        </div>
-        <div class="row">
-            <div class="col-4"></div>
             <div class="col-4 marginTop15 position-bot85">
                 <img class="block-shadow" src={{ asset('/imgs/people1.png') }} alt="people 7">
             </div>
-            <div class="col-4"></div>
         </div>
     </div>
 </section>
