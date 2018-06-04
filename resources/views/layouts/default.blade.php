e2ddae3ee11ab99ea30def991ad68d305af859a0
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-04 12:41:24 +0700

    [EDIT] master : edit web for responsive

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,6 +2,7 @@
     <head>
         <meta charset="UTF-8">
         <title>Krungsri Prime</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
         
         {{-- custom header --}}
