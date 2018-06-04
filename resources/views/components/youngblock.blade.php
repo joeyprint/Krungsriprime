6c4d37bfdcf48be5cbcc55f73a6a618f71a61e44
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-04 15:32:27 +0700

    [EDIT] master : edit column of young block component

--- resources/views/components/youngblock.blade.php
+++ resources/views/components/youngblock.blade.php
@@ -1,25 +1,25 @@
 <section id="youngblock">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-4 marginTop100">
+            <div class="col-12 col-md-4">
                 <img class="block-shadow" src={{ asset('/imgs/people1.png') }} alt="people 2">
             </div>
-            <div class="col-4 marginTop15">
+            <div class="col-12 col-md-4">
                 <img class="block-shadow" src={{ asset('/imgs/people1.png') }} alt="people 1">
             </div>
-            <div class="col-4 marginTop100">
+            <div class="col-12 col-md-4">
                 <img class="block-shadow" src={{ asset('/imgs/people1.png') }} alt="people 3">
             </div>
-            <div class="col-4 marginTop15">
+            <div class="col-12 col-md-4">
                 <img class="block-shadow" src={{ asset('/imgs/people1.png') }} alt="people 5">
             </div>
-            <div class="col-4 marginTop15 position-bot85">
+            <div class="col-12 col-md-4">
                 <img class="block-shadow" src={{ asset('/imgs/people1.png') }} alt="people 4">
             </div>
-            <div class="col-4 marginTop15">
+            <div class="col-12 col-md-4">
                 <img class="block-shadow" src={{ asset('/imgs/people1.png') }} alt="people 6">
             </div>
-            <div class="col-4 marginTop15 position-bot85">
+            <div class="col-12 col-md-4">
                 <img class="block-shadow" src={{ asset('/imgs/people1.png') }} alt="people 7">
             </div>
         </div>
