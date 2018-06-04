df1c6aef7830a6c4de15492501de6b28f16dd64f
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-04 17:58:01 +0700

    [EDIT] master : edit social card with juicer

--- resources/views/components/socialcard.blade.php
+++ resources/views/components/socialcard.blade.php
@@ -1,28 +1,15 @@
 <section id="card" class="marginTop40">
     <div class="container">
         <div class="row justify-content-center marginTop20">
-            @for($i = 1; $i <= 8; $i++)
-                @if($i > 6)
-                    @break
-                @else
-                    <div class="col-12 col-sm-6 col-lg-4">
-                        <div class="row">
-                            <div class="col-12">
-                                <img src={{ asset('/imgs/people1.png') }} alt="social blog" width="100%">
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12">
-                                <p class="whitebg black-text padding10">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's </p>
-                            </div>
-                        </div>
-                    </div>
-                @endif
-            @endfor
-        </div>
-        <div class="row justify-content-center marginTop20">
-            <div class="col-12 col-sm-6 col-lg-2 text-center">
-                <p class="btn-load padding10 load-text">..LOAD MORE..</p>
+            <div class="col-12 d-none d-md-block">
+                    <script src="https://assets.juicer.io/embed.js" type="text/javascript"></script>
+                    <link href="https://assets.juicer.io/embed.css" media="all" rel="stylesheet" type="text/css" />
+                    <ul class="juicer-feed" data-feed-id="firstmillion" data-columns="3" data-per="6"></ul>
+            </div>
+            <div class="col-12 d-md-none">
+                    <script src="https://assets.juicer.io/embed.js" type="text/javascript"></script>
+                    <link href="https://assets.juicer.io/embed.css" media="all" rel="stylesheet" type="text/css" />
+                    <ul class="juicer-feed" data-feed-id="firstmillion" data-columns="1" data-per="2"></ul>
             </div>
         </div>
     </div>
