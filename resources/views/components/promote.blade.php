060ef0bcb66f735003ca5fc84a410f4697012c43
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-04 14:31:57 +0700

    [EDIT] master : edit structure code

--- resources/views/components/promote.blade.php
+++ resources/views/components/promote.blade.php
@@ -1,42 +1,33 @@
 <section id="promote">
-    <div class="container container-size">
-        <div class="row d-block d-md-none justify-content-center paddingTop80">
+    <div class="container">
+        <div class="row justify-content-center d-block d-lg-none paddingTop80">
             <div class="col-12 text-center">
                 <h1 class="topic">KRUNGSRI PRIME</h1>
             </div>
-        </div>
-        <div class="row d-none d-md-block justify-content-end paddingTop80">
-            <div class="col-md-6 text-center">
-                <h1 class="topic">KRUNGSRI PRIME</h1>
-            </div>
-            <div class="col-md-1"></div>
-        </div>
-        <div class="row justify-content-end promote-position">
-            <div class="d-block d-md-none col-12 sub-topic">
-                <h5 class="text-center">"ล้านแรกของคุณเก็บกับกรุงศรี<br>รับความพิเศษได้ก่อนใคร”</h5>
-            </div>
-            <div class="d-none d-md-block col-md-6 sub-topic">
-                <h4 class="text-left padingTop10">"ล้านแรกของคุณเก็บกับกรุงศรี</h4>
-                <h4 class="text-right padingTop10">รับความพิเศษได้ก่อนใคร”</h4>
-            </div>
-            <div class="d-none d-md-block col-md-1"></div>
-        </div>
-        <div class="row d-none d-md-block justify-content-start">
-            <div class="col-md-1"></div>
-            <div class="col-md-4">
-                <img src={{ asset('/imgs/mobileapp.png') }} alt="mobile application" height="450px">
+            <div class="col-12 text-center">
+                <h5 class="padingTop10 sub-topic">"ล้านแรกของคุณเก็บกับกรุงศรี<br><span class="paddingTop10">รับความพิเศษได้ก่อนใคร”</span></h5>
             </div>
         </div>
-        <div class="row d-block d-md-none justify-content-center">
-            <div class="col-10">
-                <img class="mobile-pic" src={{ asset('/imgs/mobileapp.png') }} alt="mobile application" height="300px">
+        <div class="row justify-content-center">
+            <div class="col-md-1 d-none d-lg-block"></div>
+            <div class="col-12 col-lg-5 text-center paddingTop80">
+                <img src={{ asset('/imgs/mobileapp.png') }} alt="mobile application" height="300px">
             </div>
-        </div>
-        <div class="row justify-content-end promote-position">
-            <div class="col-12 col-md-6 text-justify">
+            <div class="col-12 col-lg-5 paddingTop80">
+                <div class="row justify-content-center d-none d-lg-block">
+                    <div class="col-12 text-center">
+                        <h1 class="topic">KRUNGSRI PRIME</h1>
+                    </div>
+                </div>
+                <div class="row justify-content-center d-none d-lg-block">
+                    <div class="col-12 sub-topic">
+                        <h4 class="text-left padingTop10">"ล้านแรกของคุณเก็บกับกรุงศรี</h4>
+                        <h4 class="text-right padingTop10">รับความพิเศษได้ก่อนใคร”</h4>
+                    </div>
+                </div>
                 <div class="row justify-content-center">
-                    <div class="col-12 col-md-10">
-                        <p class="description paddingTop10">
+                    <div class="col-12">
+                        <p class="description paddingTop10 text-justify">
                             ที่พร้อมพาคุณไปให้ไกลขึ้น ให้คุณรับพิเศษก่อนใคร ด้วยสิทธิประโยชน์
                             ทางด้านการเงิน และไลฟ์สไตล์ เพื่อเติมเต็มชีวิต สำหรับลูกค้าธนาคาร
                             กรุงศรีอยุธยา ที่มีเงินฝาก และ/หรือการลงทุนในกองทุน รวมของ
@@ -45,40 +36,33 @@
                         </p>
                     </div>
                 </div>
-            </div>
-            <div class="d-none d-md-block col-md-1"></div>            
-        </div>
-        <div class="row justify-content-end promote-position">
-            <div class="col-12 col-md-6">
                 <div class="row justify-content-center">
-                    <div class="col-md-10 text-center font-weight-bold">
+                    <div class="col-12 text-center font-weight-bold">
                         <p class="description paddingTop10">
                             ดาวน์โหลด แอปพลิเคชั่น KRUNGSRI PRIME<br>
                             เพื่อรับเอกสิทธิ์เหนือระดับก่อนใครง่ายๆ ด้วยปลายนิ้วสัมผัส
                         </p>
                     </div>
                 </div>
-            </div>
-            <div class="d-none d-md-block col-md-1"></div>            
-        </div>
-        <div class="row justify-content-end promote-position">
-            <div class="col-12 col-md-6">
                 <div class="row justify-content-center">
-                    <div class="col-12 col-md-2 text-center">
-                        <img src={{ asset('/imgs/app-icon.png') }} alt="application icon" height="40px">
-                    </div>
-                    <div class="col-6 col-md-3 text-center">
-                        <img src={{ asset('/imgs/appstore.png') }} alt="appstore icon" height="40px">                        
-                    </div>
-                    <div class="col-6 col-md-3  text-center">
-                        <img src={{ asset('/imgs/googleP.png') }} alt="ggplay icon" height="40px">                        
+                    <div class="col-12">
+                        <div class="row justify-content-center">
+                            <div class="col-12 col-lg-2 text-center marginTop10">
+                                <img src={{ asset('/imgs/app-icon.png') }} alt="application icon" height="40px">
+                            </div>
+                            <div class="col-6 col-lg-4 text-center marginTop10">
+                                <img src={{ asset('/imgs/appstore.png') }} alt="appstore icon" height="40px">                        
+                            </div>
+                            <div class="col-6 col-lg-4 text-center marginTop10">
+                                <img src={{ asset('/imgs/googleP.png') }} alt="ggplay icon" height="40px">                        
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="d-none d-md-block col-md-1"></div>            
         </div>
-        <div class="row justify-content-center btn-position">
-            <div class="col-12 col-md-4 text-center">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-4 text-center">
                 <p class="btn-goweb">เข้าสู่เว็บไซต์ KRUNGSRI PRIME</p>
             </div>
         </div>
