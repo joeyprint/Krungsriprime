4deb9d8f4f2887601654b1da14d92009ed0ea25d
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-04 15:14:44 +0700

    [EDIT] master : edit structure middle content component

--- resources/views/components/midcontent.blade.php
+++ resources/views/components/midcontent.blade.php
@@ -1,8 +1,7 @@
 <section id="midcontent">
     <div class="container">
-        <div class="row">
-            <div class="col-md-2"></div>
-            <div class="col-md-8 text-center line margin10">
+        <div class="row justify-content-center">
+            <div class="col-10 col-sm-6 col-md-8 text-center line margin10">
                 <p class="margin20 text">
                     ร่วมแชร์โมเม้นต์ก้าวที่ล้าน ที่เป็นก้าวแห่งการเติบโต<br>
                     และก้าวแรกของความสำเร็จของคุณ เพียงแชร์เรื่องราวพร้อมติดแฮชแท็ก<br>
