130a1acfd792485a0f5b503a423a37b082672867
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-04 15:31:47 +0700

    [EDIT] master : edit container of banner to container fluid

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,19 +1,23 @@
-<div class="row">
-    <div class="col-12">
-        <video src=""></video><p>video for Krungsri Prime</p>
-    </div>
-</div>
-<div class="row scroll-index">
-    <div class="col-12 text-center">
-        <div class="row">
+<section id="banner">
+    <div class="container-fluid">
+        <div class="row justify-content-center">
             <div class="col-12">
-                <img src="/imgs/down.png" alt="scroll-down vector">
+                <video src=""></video><p>video for Krungsri Prime</p>
             </div>
         </div>
-        <div class="row">
-            <div class="col-12">
-                <p class="scroll-text">SCROLL DOWN</p>
+        <div class="row justify-content-center scroll-index">
+            <div class="col-12 text-center">
+                <div class="row">
+                    <div class="col-12">
+                        <img src="/imgs/down.png" alt="scroll-down vector">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <p class="scroll-text">SCROLL DOWN</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
+</section>
