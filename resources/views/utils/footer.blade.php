d9aff7a42cfa750dbdf583f9619e9772dc5b36f8
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-04 15:31:00 +0700

    [EDIT] master : edit container of footer -> container fluid

--- resources/views/utils/footer.blade.php
+++ resources/views/utils/footer.blade.php
@@ -1,7 +1,9 @@
 <section id="footer">
-    <div class="row">
-        <div class="col-12 text-center yellowbg">
-            <p class="black-text padding10 margin0">Copyright &copy; Krungsri prime</p>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-12 text-center yellowbg">
+                <p class="black-text paddingTB10 margin0">Copyright &copy; Krungsri prime</p>
+            </div>
         </div>
     </div>
 </section>
