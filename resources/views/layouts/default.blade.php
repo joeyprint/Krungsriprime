1fe111d6cefd5fc71077a808eb96679f681df933
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-05 10:52:19 +0700

    [ADD] master : add theme owl carousel into layout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
         <link rel="stylesheet" href={{ asset('/css/owl.carousel.min.css') }}>
+        <link rel="stylesheet" href={{ asset('/css/owl.theme.default.min.css') }}>
 
         {{-- custom header --}}
         @yield('custom_head')
