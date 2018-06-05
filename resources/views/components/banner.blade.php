21efe8d4c57354ead863695b25234f750a139b71
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-05 09:02:04 +0700

    [EDIT] master : edit position of video and button

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,39 +1,16 @@
 <section id="banner">
-    <div class="container-fluid padding0 video-position">
-        <div class="row no-gutters justify-content-center">
+    <div class="container-fluid padding0">
+        <div class="row no-gutters justify-content-center video-position">
             <div class="col-12">
-                <video class="d-none d-md-block video-width" src={{ asset('/imgs/Firstmillion_banner.mp4') }} autoplay></video>
+                <video class="d-none d-md-block video-width" src={{ asset('/imgs/Firstmillion_banner.mp4') }} preload="auto" loop autoplay></video>
                 <video class="d-md-none video-width" src={{ asset('/imgs/Firstmillion_banner_Mobile.mp4') }} autoplay></video>
             </div>
         </div>
-        <div class="row justify-content-center marginBot30 video-position scroll-index no-gutters">
+        <div class="row justify-content-center btn-position scroll-index no-gutters">
             <div class="col-12 text-center">
                 <img src="/imgs/down.png" alt="scroll-down vector">
                 <p class="marginTop10">SCROLL DOWN</p>
             </div>
         </div>
     </div>
-    {{-- <div class="container">
-        <div class="row justify-content-center title-position">
-            <div class="col-10 col-md-6 line">
-                <div class="row justify-content-center">
-                    <div class="col-12">
-                        <p class="text-center title-text yellow-text marginTop30"># F I R S T M I L L I O N</p>
-                    </div>
-                </div>
-                <div class="row justify-content-center">
-                    <div class="col-11">
-                        <p class="text-center text marginBot30">
-                            โมเมนต์สำคัญอย่างหนึ่งของชีวิตที่คนเรามักจดจำ
-                            คือโมเมนต์การเปลี่ยนแปลงที่เป็นก้าวแห่งการเติบโตของชีวิต
-                            อาทิ โมเมนต์การเติบโตของเงินเก็บที่เปลี่ยนจาก 6 หลัก สู่ 7 หลัก
-                            นี่คือช่วงเวลาที่มีความหมายน่าจดจำ KRUNGSRI PRIME
-                            ให้ความสำคัญกับก้าวแห่งการเติบโตทางการเงินของคุณ 
-                            และเป็นบริการที่จะมอบความพิเศษให้กับลูกค้าก่อนใคร
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> --}}
 </section>
