f1f64a06b1d7ada47d662f1f87440dff9e906349
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-05 10:18:44 +0700

    [FIX] master : fix bug size video banner

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -3,7 +3,7 @@
         <div class="row no-gutters justify-content-center">
             <div class="col-12">
                 <video class="d-none d-md-block video-width" src={{ asset('/imgs/Firstmillion_banner.mp4') }} preload="auto" loop autoplay></video>
-                <video class="d-md-none video-widthvh-100" src={{ asset('/imgs/Firstmillion_banner_Mobile.mp4') }} autoplay></video>
+                <video class="d-md-none video-width" src={{ asset('/imgs/Firstmillion_banner_Mobile.mp4') }} autoplay></video>
             </div>
         </div>
         <div class="row justify-content-center btn-position scroll-index no-gutters">
