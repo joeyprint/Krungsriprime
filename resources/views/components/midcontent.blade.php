3821a31973ebcaa0b50de74d026e24c224485e93
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-08 13:32:48 +0700

    [ADD] master : add animation into middle context

--- resources/views/components/midcontent.blade.php
+++ resources/views/components/midcontent.blade.php
@@ -1,14 +1,25 @@
 <section id="midcontent">
     <div class="container marginBot80">
         <div class="row justify-content-center">
-            <div class="col-11 col-sm-8 text-center line margin10">
+            <div class="col-11 col-sm-8 text-center line margin10 d-block d-md-none">
                 <p class="margin20 text">
                     ร่วมแชร์โมเม้นต์ก้าวที่ล้าน ที่เป็นก้าวแห่งการเติบโต<br>
                     และก้าวแรกของความสำเร็จของคุณ เพียงแชร์เรื่องราวพร้อมติดแฮชแท็ก<br>
-                    <span class="yellow-text hash-text">
+                    <span class="yellow-text hash-text animated fadeInUp">
                         # F <span class="white-text">I</span> R S T M I L L I O N
                     </span>
                 </p>
+            </div>
+            <div class="col-11 col-sm-8 text-center margin10 d-none d-md-block">
+                <p class="line text-center aos-init" data-aos="zoom-in"></p>
+                <p class="margin20 text aos-init" data-aos="fade-down">
+                    ร่วมแชร์โมเม้นต์ก้าวที่ล้าน ที่เป็นก้าวแห่งการเติบโต<br>
+                    และก้าวแรกของความสำเร็จของคุณ เพียงแชร์เรื่องราวพร้อมติดแฮชแท็ก<br>
+                </p>
+                <p class="yellow-text hash-text aos-init" data-aos="fade-up">
+                    # F <span class="white-text">I</span> R S T M I L L I O N
+                </p>
+                <p class="line text-center aos-init" data-aos="zoom-in"></p>
             </div>
         </div>
     </div>
