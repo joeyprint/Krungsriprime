bdde9683733c47e79ec1ed141ed279f43266965c
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-11 16:32:59 +0700

    [EDIT] master : edit length of social card

--- resources/views/components/socialcard.blade.php
+++ resources/views/components/socialcard.blade.php
@@ -4,12 +4,12 @@
             <div class="col-12 d-none d-md-block">
                     <script src="https://assets.juicer.io/embed.js" type="text/javascript"></script>
                     <link href="https://assets.juicer.io/embed.css" media="all" rel="stylesheet" type="text/css" />
-                    <ul class="juicer-feed" data-feed-id="firstmillion" data-columns="3" data-per="6"></ul>
+                    <ul class="juicer-feed" data-feed-id="firstmillion" data-columns="3" data-per="6" data-truncate="200"></ul>
             </div>
             <div class="col-12 d-md-none">
                     <script src="https://assets.juicer.io/embed.js" type="text/javascript"></script>
                     <link href="https://assets.juicer.io/embed.css" media="all" rel="stylesheet" type="text/css" />
-                    <ul class="juicer-feed" data-feed-id="firstmillion" data-columns="1" data-per="2"></ul>
+                    <ul class="juicer-feed" data-feed-id="firstmillion" data-columns="1" data-per="2" data-truncate="200"></ul>
             </div>
         </div>
     </div>
