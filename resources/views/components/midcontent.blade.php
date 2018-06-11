94f175f405fbb17962162a53627be41327dee6a0
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-06-11 16:36:27 +0700

    [EDIT] master : edit aos offset

--- resources/views/components/midcontent.blade.php
+++ resources/views/components/midcontent.blade.php
@@ -11,15 +11,15 @@
                 </p>
             </div>
             <div class="col-11 col-sm-8 text-center margin10 d-none d-lg-block">
-                <p class="line" data-aos="fade-center" data-aos-easing="linear"></p>
-                <p class="margin20 text" data-aos="header-down" data-aos-easing="linear">
+                <p class="line" data-aos="fade-center" data-aos-easing="linear" data-aos-offset="10"></p>
+                <p class="margin20 text" data-aos="header-down" data-aos-easing="linear" data-aos-offset="10" >
                     ร่วมแชร์โมเม้นต์ก้าวที่ล้าน ที่เป็นก้าวแห่งการเติบโต<br>
                     และก้าวแรกของความสำเร็จของคุณ เพียงแชร์เรื่องราวพร้อมติดแฮชแท็ก<br>
                 </p>
-                <p class="yellow-text hash-text" data-aos="header-up" data-aos-easing="linear">
+                <p class="yellow-text hash-text" data-aos="header-up" data-aos-easing="linear" data-aos-offset="50">
                     # F <span class="white-text">I</span> R S T M I L L I O N
                 </p>
-                <p class="line text-center aos-init" data-aos="fade-center" data-aos-easing="linear"></p>
+                <p class="line text-center aos-init" data-aos="fade-center" data-aos-easing="linear" data-aos-offset="10"></p>
             </div>
         </div>
     </div>
